dd744a0d0af9c8f1f02986e281330da08fa0b1de
stictt <stic_t@mail.ru>
2022-09-10 16:01:48 +0300

    Create SQL file

---
+++ Select 2.sql
@@ -0,0 +1,5 @@
+Select Tab1.Name 
+From (Select Top 1 AVG(EMPLOYEE.SALARY) as SalaryAvg ,DEPARTMENT.Name
+From DEPARTMENT inner Join EMPLOYEE on EMPLOYEE.DEPARTMENT_ID = DEPARTMENT.ID
+Group By DEPARTMENT.Name 
+Order By SalaryAvg Desc) as Tab1
